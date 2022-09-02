9e5d121b4d7fd0098c101ae284727c5c07a7b54c
Prajituric <bugheanudanutandrei@gmail.com>
2022-09-02 21:35:16 +0300

    optimize

---
+++ functionality/songs.js
@@ -0,0 +1,82 @@
+const songs = [
+  {
+    displayName: "I'm not a Saint",
+    artist: "Billy Raffoul",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141629/song-1_rvnkqi.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141597/song-1_zosune.jpg",
+  },
+  {
+    displayName: "You're somebody else",
+    artist: "Flora Cash",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141632/song-2_dhiqss.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141598/song-2_z3xal1.jpg",
+  },
+  {
+    displayName: "O mie de pahare",
+    artist: "White Mahala",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141630/song-3_pt3ajf.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141597/song-3_mxzzch.jpg",
+  },
+  {
+    displayName: "Same Old Energy",
+    artist: "Kiki Rockwell",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141631/song-4_sf63a1.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141597/song-4_qc1fsz.jpg",
+  },
+  {
+    displayName: "Dead Inside",
+    artist: "Younger Hunger",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141631/song-5_bnohik.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141597/song-5_xfnhng.jpg",
+  },
+  {
+    displayName: "Insuperabile",
+    artist: "Rkomi",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141632/song-6_tx5hts.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141597/song-6_oxqu5d.jpg",
+  },
+  {
+    displayName: "Princesses Don't Cry",
+    artist: "Carys",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141632/song-7_ld1gpg.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141598/song-7_aoijbc.jpg",
+  },
+  {
+    displayName: "High Enough",
+    artist: "K. Flay",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141631/song-8_gxvfr9.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141598/song-8_c3wpkx.jpg",
+  },
+  {
+    displayName: "Hope",
+    artist: "The Chainsmokers",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141632/song-9_oimnaz.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141598/song-9_n0jn4h.jpg",
+  },
+  {
+    displayName: "Amnesia",
+    artist: "5 Seconds of Summer",
+    songs:
+      "https://res.cloudinary.com/devul/video/upload/v1662141633/song-10_wite0c.mp4",
+    image:
+      "https://res.cloudinary.com/devul/image/upload/v1662141598/song-10_ucttdl.jpg",
+  },
+];
--- script.js
+++ script.js
@@ -24,8 +24,8 @@ playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
 function loadSong(song) {
   title.textContent = song.displayName;
   artist.textContent = song.artist;
-  music.src = `music/${song.name}.mp3`;
-  image.src = `img/${song.name}.jpg`;
+  music.src = song.songs;
+  image.src = song.image;
 }
 
 // Current Song
